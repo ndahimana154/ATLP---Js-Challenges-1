d2311f9debeea64a83eb475172df3f8e07e52b57
Ndahimana Bonheur <ndahimana154@gmail.com>
2024-03-22 12:40:06 +0200

    Made the Sorting

---
+++ src/filterObject.ts
@@ -0,0 +1,47 @@
+    interface PersonsInfo {
+    "First-name": string;
+    "Second-name": string;
+    age: number;
+    "gender": string;
+    }
+    interface Result {
+    male: PersonsInfo[];
+    female: PersonsInfo[];
+    }
+
+    function filterByGender(personsArray: string[]): object {
+    const result: Result = {
+        male: [],
+        female: [],
+    };
+
+    for (const person of personsArray) {
+        var [fullNames, age, gender] = person
+        .split(",")
+        .map((part: any) => part.trim());
+        const [firstname, secondname] = fullNames.split(" ");
+        const personsInfo: PersonsInfo = {
+        "First-name": firstname,
+        "Second-name": secondname,
+        age: parseInt(age),
+        "gender": gender.toLowerCase(),
+        };
+
+        if (personsInfo.gender == "male") result.male.push(personsInfo);
+        else result.female.push(personsInfo);
+    }
+    return result;
+    }
+    
+    const People = [
+    "Patrick wyne, 30, male",
+    "lil wyne, 32, male",
+    "Eric mimi, 21, female",
+    "Dodos deck, 21,male",
+    "Alian Dwine, 22, male",
+    "Patrick wyne, 33, male",
+    "Patrick wyne, 10,male",
+    "Patrick wyne, 40,male",
+    ];
+
+    console.log(filterByGender(People));
---
+++ src/sorting.ts
@@ -0,0 +1,19 @@
+function sortFx(arr: number[]): number[] {
+    const length = arr.length;
+  
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - 1 - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          const temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+  
+    return arr;
+  }
+  
+  const numbers: number[] = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+  const sortedNumbers = sortFx(numbers);
+  console.log(sortedNumbers);
